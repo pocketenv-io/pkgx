7c7c9a53e843f96943118aaaacec6fc1edb87545
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-02 19:06:54 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pkgx Workspace
+
+This is a template workspace with pkgx. It is a starting point for creating a new workspace.
+
---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "packages" {
+  type = list(string)
+  description = "Packages to install"
+  default = []
+}
+
+variable "hostname" {
+  type = string
+  description = "Hostname of the container"
+  default = null
+} 
+
+variable "image" {
+  type = string
+  description = "Image to use, required if context is not set"
+  default = "ghcr.io/pocketenv-io/pkgx:main"
+}
+
+variable "workspace_name" {
+  type = string
+  description = "Name of the workspace"
+  default = "pkgx-workspace"
+}
+
+variable "user" {
+  type = string
+  description = "User to create"
+  default = "coder"
+}
+
+variable "context" {
+  type = string
+  description = "Context to use"
+  default = null
+}
+
+variable "volumes" {
+  type = map(string)
+  description = "Volumes to mount"
+  default = {
+    pkgx-workspace = "/home/coder"
+  }
+}
